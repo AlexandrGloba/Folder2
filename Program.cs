18d5feef355ccb6bc6c0fe4a8f08ef7fa20e520c
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2023-03-03 11:15:47 +0300

    add task1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Введите колличество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    Console.Write($"Введите число : ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Введено чисел больше нуля: {count}");
